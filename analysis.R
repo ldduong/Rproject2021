9ebc4e3521ebd5471dbe2a7325499009ab41a4f9
Loan Duong <ldduong@nd.edu>
2021-11-29 11:11:20 -0500

    Script files created

---
+++ analysis.R
@@ -0,0 +1,5 @@
+#R Project, Biocomputing Fall 2021
+#Loan Duong
+#Second script: Uses the source function to load the functions defined in first script, compiles
+#all data into a CSV file, process the data included in the entire data set to answer the 
+#questions, and provide graphical evidence. 
---
+++ supportingFunctions.R
@@ -0,0 +1,3 @@
+#R Project, Biocomputing Fall 2021
+#Loan Duong
+#First script: a number of custom functions
